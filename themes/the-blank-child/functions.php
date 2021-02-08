7c975287db56fcc605f24365e92f73c072d0af64
y-u-y-a <yuya.ymd00@gmail.com>
2021-02-08 19:41:51 +0900

    init cocoon the-blank

---
+++ themes/the-blank-child/functions.php
@@ -0,0 +1,6 @@
+<?php
+    # 子テーマの読み込み
+    add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+    function theme_enqueue_styles() {
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    }
